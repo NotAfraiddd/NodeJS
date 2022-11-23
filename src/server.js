8c0467ca76f65d34cb84439cc02ba9288fe44268
ChiBao <99703812+NotAfraiddd@users.noreply.github.com>
2022-11-23 18:30:32 +0700

    fix lai 100%, su dung handlebars

--- src/server.js
+++ src/server.js
@@ -1,33 +1,32 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
-const path = require('path')
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname,'public')))
 
+app.use(express.urlencoded({
+    extended: true
+}))
+// urlencoded(), xử lí dạng form
+app.use(express.json())
+// json() xử lí từ js đưa lên
 
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // template engine
-app.set('view engine','ejs')
-app.set('views',path.join(__dirname,'resources/views'))
+app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/',(req,res) =>{
-    res.render('home')
-})
+// routes init
+route(app)
 
-app.get('/news',(req,res) =>{
-    res.render('news')
-})
 
-app.get('/search', (req, res) => {
-    res.render('search');
-});
-
-app.post('/search', (req, res) => {
-    res.send('');
-});
 
 app.listen(port,() => console.log('100%'))
